e072d36c43d349c13bc328e28628660460231655
SergeyXP <s.pusthotin@gmail.com>
2023-11-25 21:01:41 +0400

    hw02

---
+++ hw02/Homework02/Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Software Design");
+            Console.WriteLine("Homework 01");
+
+            Console.WriteLine("Описание проблемы:" +
+                "Есть класс Summator." +
+                "Вносить изменения в этот класс нельзя." +
+                "Требуется добавить логирование до и после вызовов метода Execute," +
+                "Воспользуемся паттерном Декоратор");
+
+
+            var summator = new WrapperSummator(1, 2);
+
+            var value = summator.GetResult();
+
+            Console.WriteLine($"Результат: {value}");
+        }
+    }
+}
---
+++ hw02/Homework02/Singleton/Summator.cs
@@ -0,0 +1,30 @@
+﻿namespace Singleton
+{
+    class Summator
+    {
+        public int A { get; }
+        public int B { get; }
+
+        public Summator(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+        public int GetResult()
+        {
+            return A + B;
+        }
+
+    }
+
+    class WrapperSummator(int a, int b) : Summator(a, b)
+    {
+        public new int GetResult()
+        {
+            Console.WriteLine($"logger: До вызова a={A}, b={B}");
+            var result = base.GetResult();
+            Console.WriteLine($"logger: После вызова a={A}, b={B}, result={result}");
+            return result;
+        }
+    }
+}
