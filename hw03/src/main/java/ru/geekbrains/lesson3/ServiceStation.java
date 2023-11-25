0ae1e36828c1719379034ab199049da7e849a765
SergeyXP <s.pusthotin@gmail.com>
2023-11-25 21:15:01 +0400

    hw02

---
+++ hw03/src/main/java/ru/geekbrains/lesson3/ServiceStation.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.lesson3;
+
+public class ServiceStation implements Refueling, Wiping{
+    @Override
+    public void fuel(FuelType fuelType) {
+
+    }
+
+    @Override
+    public void wipMirrors() {
+
+    }
+
+    @Override
+    public void wipWindshield() {
+
+    }
+
+    @Override
+    public void wipHeadlights() {
+
+    }
+}
