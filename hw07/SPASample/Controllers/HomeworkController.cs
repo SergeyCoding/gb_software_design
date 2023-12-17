99f76e691ff12b0d3c431af8d9e56fa5ae756007
SergeyXP <s.pusthotin@gmail.com>
2023-12-17 10:45:04 +0400

    hw07

---
+++ hw07/SPASample/Controllers/HomeworkController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPASample.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
