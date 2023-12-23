5b3c9c940758d4e028ecc9f902d1cf3781befab7
SergeyXP <s.pusthotin@gmail.com>
2023-12-23 10:02:07 +0400

    hw10

---
+++ hw10/ClinicService/Models/Requests/CreatePetRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ClinicService.Models.Requests
+{
+    public class CreatePetRequest
+    {
+        //public int PetId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
