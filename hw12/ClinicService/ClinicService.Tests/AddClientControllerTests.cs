9c4f2a18fd8fa10be1ed183fc67a8a00830fa886
SergeyXP <s.pusthotin@gmail.com>
2024-01-13 15:43:10 +0400

    hw12

--- hw12/ClinicService/ClinicService.Tests/AddClientControllerTests.cs
+++ hw12/ClinicService/ClinicService.Tests/AddClientControllerTests.cs
@@ -7,54 +7,54 @@ using Moq;
 
 namespace ClinicService.Tests
 {
-    public class ClientControllerTests
+    public class AddClientControllerTests
     {
         private ClientController _clientController;
         private Mock<IClientRepository> _mockClientRepository;
 
-        public ClientControllerTests()
+        public AddClientControllerTests()
         {
             _mockClientRepository = new Mock<IClientRepository>();
             _clientController = new ClientController(_mockClientRepository.Object);
         }
 
 
-        [Fact]
-        public void GetAllClientsTest()
-        {
-            // [1] Подготовка данных для тестирования
+        //[Fact]
+        //public void GetAllClientsTest()
+        //{
+        //    // [1] Подготовка данных для тестирования
 
-            List<Client> clientsList = new List<Client>();
-            clientsList.Add(new Client());
-            clientsList.Add(new Client());
-            clientsList.Add(new Client());
+        //    var clientsList = new List<Client>();
+        //    clientsList.Add(new Client());
+        //    clientsList.Add(new Client());
+        //    clientsList.Add(new Client());
 
-            _mockClientRepository
-                .Setup(repository => repository.GetAll())
-                .Returns(clientsList);
+        //    _mockClientRepository
+        //        .Setup(repository => repository.GetAll())
+        //        .Returns(clientsList);
 
-            // [2] Исполнение тестируемого метода
+        //    // [2] Исполнение тестируемого метода
 
 
-            var operationResult = _clientController.GetAll();
+        //    var operationResult = _clientController.GetAll();
 
-            // [3] Подготовка эталонного результата и проверка результата
-            Assert.IsType<OkObjectResult>(operationResult.Result);
+        //    // [3] Подготовка эталонного результата и проверка результата
+        //    Assert.IsType<OkObjectResult>(operationResult.Result);
 
-            var result = (OkObjectResult)operationResult.Result;
-            Assert.IsAssignableFrom<List<Client>>(result.Value);
+        //    var result = (OkObjectResult)operationResult.Result;
+        //    Assert.IsAssignableFrom<List<Client>>(result.Value);
 
-            _mockClientRepository.Verify(repository =>
-            repository.GetAll(), Times.AtLeastOnce);
+        //    _mockClientRepository.Verify(repository =>
+        //    repository.GetAll(), Times.AtLeastOnce);
 
-        }
+        //}
 
         public static object[][] CorrectCreateClientData =
         {
             new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов1", "Станислав1", "Андреевич1" },
             new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов2", "Станислав2", "Андреевич2" },
             //new object[] { new DateTime(2013, 1, 22), "AA1 B11222", "Иванов3", "Станислав3", "Андреевич3" },
-            new object[] { new DateTime(1980, 1, 22), "AA1 B11222", "Иванов4", "Станислав4", "Андреевич4" },
+            new object[] { new DateTime(2018, 1, 22), "AA1 B11222", "Иванов4", "Станислав4", "Андреевич4" },
             new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов5", "Станислав5", "Андреевич5" },
             //new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов", "", "Андреевич" },
             new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов6", "Станислав6", "Андреевич6" },
@@ -70,14 +70,14 @@ namespace ClinicService.Tests
         {
             // [1] Подготовка данных для тестирования
 
-            CreateClientRequest createClientRequest = new CreateClientRequest();
+            var createClientRequest = new CreateClientRequest();
             createClientRequest.Birthday = birthday;
             createClientRequest.Document = document;
             createClientRequest.SurName = surName;
             createClientRequest.FirstName = firstName;
             createClientRequest.Patronymic = patronymic;
 
-            int res = 1;
+            var res = 1;
 
             _mockClientRepository
                 .Setup(repository => repository.Create(It.IsNotNull<Client>()))
